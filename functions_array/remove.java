a17dd040f37f6d2880d963d2daed0ed383868dbf
Sarthak090805 <sarthaksaxenaa05@gmail.com>
2025-09-20 01:17:28 +0530

    method name modified

--- functions_array/remove.java
+++ functions_array/remove.java
@@ -1,9 +1,9 @@
 class remove{
     public static void main(String[] args) {
         int arr[] = {0,1,2,2,3,0,4,2}, val = 2;
-        System.out.println(remove(arr,val));
+        System.out.println(removee(arr,val));
     }
-    public static int remove(int[] arr , int val)
+    public static int removee(int[] arr , int val)
     {
         int k=0;
         int n = arr.length;

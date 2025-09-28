0b054047c218ec24988c11602df32d5d207f052a
Sarthak090805 <sarthaksaxenaa05@gmail.com>
2025-09-29 00:52:03 +0530

    patterns

---
+++ PATTERNS/pattern17.java
@@ -0,0 +1,39 @@
+public class pattern17 {
+    public static void main(String args[]){
+        int n=5;
+        //UPPER
+        for(int i=0;i<n;i++){
+            //star
+            for(int j=n-i;j>0;j--){
+                System.out.print("*");
+            }
+            //space
+            for(int j=0;j<2*i;j++)
+            {
+                System.out.print(" ");
+            }
+            //star
+            for(int j=n-i;j>0;j--){
+                System.out.print("*");
+            }
+        System.out.println();
+        }
+        //LOWER
+        for(int i=n-1;i>=0;i--){
+            //star
+            for(int j=n-i;j>0;j--){
+                System.out.print("*");
+            }
+            //space
+            for(int j=0;j<2*i;j++)
+            {
+                System.out.print(" ");
+            }
+            //star
+            for(int j=n-i;j>0;j--){
+                System.out.print("*");
+            }
+        System.out.println();
+        }
+    }
+}

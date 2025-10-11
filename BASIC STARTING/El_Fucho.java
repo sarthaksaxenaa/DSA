a59a9345967b8acb52bfac068d696147c9baa24a
Sarthak090805 <sarthaksaxenaa05@gmail.com>
2025-10-11 10:27:15 +0530

    El Fucho

---
+++ BASIC STARTING/El_Fucho.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class El_Fucho {
+    
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        for (int i = 0; i < n; i++) 
+        {
+            int a=sc.nextInt();
+            System.out.println(2*a-2);
+        }
+    }
+}

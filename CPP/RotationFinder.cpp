193cc09a9abc77a435f4d9b9e7c76469cc70537c
Sarthak090805 <sarthaksaxenaa05@gmail.com>
2025-10-16 03:57:45 +0530

    Find the numbner of rotations in the array

---
+++ CPP/RotationFinder.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class RotationFinder {
+public:
+    int findKRotation(vector<int> &nums) {
+        int low = 0, high = nums.size() - 1;
+        while (low < high) {
+            int mid = low + ((high - low) >> 1);
+            if (nums[mid] > nums[high]) low = mid + 1;
+            else high = mid;
+        }
+        return low;
+    }
+};
+
+int main() {
+    RotationFinder obj;
+    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    cout << obj.findKRotation(nums);
+}

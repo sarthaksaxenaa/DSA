3e284079f0552c9a956fd40d4cfb02e6e7e5d5f7
Sarthak090805 <sarthaksaxenaa05@gmail.com>
2025-10-30 23:42:13 +0530

    Longest Common Prefix Solution

---
+++ CPP/longestCommonPrefix.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution14 {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()) return "";
+        string prefix = strs[0];
+        for(int i = 1; i < strs.size(); i++) {
+            string temp = "";
+            for(int j = 0; j < min(prefix.size(), strs[i].size()); j++) {
+                if(prefix[j] == strs[i][j]) temp += prefix[j];
+                else break;
+            }
+            prefix = temp;
+            if(prefix == "") break;
+        }
+        return prefix;
+    }
+};
+
+int main() {
+    Solution14 obj;
+    vector<string> strs = {"flower", "flow", "flight"};
+    cout << obj.longestCommonPrefix(strs);
+}

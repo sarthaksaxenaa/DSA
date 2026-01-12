f9549d4f5cbc8b94ed482441ba3185f80aee152a
Sarthak090805 <sarthaksaxenaa05@gmail.com>
2026-01-12 22:40:28 +0530

    Stock Spanner code

---
+++ STACK/Stock_Spanner.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class StockSpanner {
+    stack<pair<int,int>> st;
+public:
+    StockSpanner() {}
+
+    int next(int price){
+        int span=1;
+        while(!st.empty() && st.top().first<=price){
+            span+=st.top().second;
+            st.pop();
+        }
+        st.push({price,span});
+        return span;
+    }
+};
+
+int main(){
+    int n;
+    cin>>n;
+    StockSpanner obj;
+    while(n--){
+        int price;
+        cin>>price;
+        cout<<obj.next(price)<<" ";
+    }
+    return 0;
+}

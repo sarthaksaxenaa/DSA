f191b62fbbd715aea4c968568ffa24a6c89929e0
Sarthak090805 <sarthaksaxenaa05@gmail.com>
2026-01-15 12:12:03 +0530

    LCA of BT code

---
+++ TREE/LCA_BT.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x):val(x),left(NULL),right(NULL){}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL || root==p || root==q) return root;
+
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
+
+        if(left && right) return root;
+        return left?left:right;
+    }
+};
+
+int main(){
+    TreeNode* root=new TreeNode(3);
+    root->left=new TreeNode(5);
+    root->right=new TreeNode(1);
+    root->left->left=new TreeNode(6);
+    root->left->right=new TreeNode(2);
+    root->right->left=new TreeNode(0);
+    root->right->right=new TreeNode(8);
+
+    TreeNode* p=root->left;
+    TreeNode* q=root->right;
+
+    Solution s;
+    TreeNode* ans=s.lowestCommonAncestor(root,p,q);
+    cout<<ans->val;
+    return 0;
+}

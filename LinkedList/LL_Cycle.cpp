8fae8562c452a1220c8774986c9b1c3cac85e69a
Sarthak090805 <sarthaksaxenaa05@gmail.com>
2026-01-23 23:29:18 +0530

    Linked List cycle code

---
+++ LinkedList/LL_Cycle.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode{
+    int val;
+    ListNode* next;
+    ListNode(int x):val(x),next(NULL){}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head){
+        ListNode* slow=head;
+        ListNode* fast=head;
+
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+};
+
+int main(){
+    ListNode* head=new ListNode(3);
+    head->next=new ListNode(2);
+    head->next->next=new ListNode(0);
+    head->next->next->next=new ListNode(-4);
+
+    head->next->next->next->next=head->next;
+
+    Solution s;
+    cout<<s.hasCycle(head);
+    return 0;
+}

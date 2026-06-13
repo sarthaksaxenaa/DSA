2cfed9765ad93ffb9c57dacdf2a6f9a407a7d1bc
Sarthak090805 <sarthaksaxenaa05@gmail.com>
2026-06-13 11:58:06 +0530

    A Domino piling cp31 code.

---
+++ CP31_SHEET/A_Domino_piling.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void solve(){
+    int a,b; 
+    cin >> a >> b;
+    
+    int d = (a*b) / 2;
+
+    cout << d << "\n";
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+        solve();
+    return 0;
+}

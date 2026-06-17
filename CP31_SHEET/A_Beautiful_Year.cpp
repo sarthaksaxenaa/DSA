ab98b5f4231cc6aae550c3cc8bfc049529682156
Sarthak090805 <sarthaksaxenaa05@gmail.com>
2026-06-17 13:55:37 +0530

    A Beautiful Year  cp31 code.

---
+++ CP31_SHEET/A_Beautiful_Year.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void solve(int n){
+    while(true){
+        n++;
+
+        int a = n;
+
+        int q = a % 10;
+        a /= 10;
+
+        int w = a % 10;
+        a /= 10;
+
+        int e = a % 10;
+        a /= 10;
+
+        int r = a % 10;
+
+        if(q!=w && q!=e && q!=r &&
+           w!=e && w!=r &&
+           e!=r){
+            cout << n << "\n";
+            return;
+        }
+    }
+}
+
+int32_t main(){
+    int n; cin >> n;
+
+    solve(n);
+
+    return 0;
+}

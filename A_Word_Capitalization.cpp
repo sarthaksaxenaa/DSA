6e5e0a10f9bd0625ddb66e744e6b75032a679753
Sarthak090805 <sarthaksaxenaa05@gmail.com>
2026-07-04 11:04:31 +0530

    A Word Capitalization cp31 sheet code.

---
+++ A_Word_Capitalization.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void solve(){
+    string s;
+    cin >> s;
+    
+    s[0] = toupper(s[0]);
+
+    cout << s;
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+        solve();
+    return 0;
+}

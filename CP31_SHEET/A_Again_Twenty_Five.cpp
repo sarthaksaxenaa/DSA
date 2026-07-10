709be8c649453246cfc15ca981bb063eae448c15
Sarthak090805 <sarthaksaxenaa05@gmail.com>
2026-07-11 02:10:15 +0530

    A Again Twenty Five cp31 sheet code.

---
+++ CP31_SHEET/A_Again_Twenty_Five.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void solve(){
+    int n;
+    cin >> n;
+
+    cout << 25;
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+        solve();
+    return 0;
+}

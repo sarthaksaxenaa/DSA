7643ea6d79b9bf8f9e40a83775edd4a28de63815
Sarthak090805 <sarthaksaxenaa05@gmail.com>
2026-07-18 14:16:17 +0530

    A Stair Peak or Neither cp31 sheet code.

---
+++ CP31_SHEET/A_Stair_Peak_or_Neither.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void solve(){
+    int a,b,c;
+    cin >> a >> b >> c;
+
+    if(a<b && b<c) cout << "STAIR" << "\n";
+    else if(a<b && b>c) cout << "PEAK" << "\n";
+    else cout << "NONE" << "\n";
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}

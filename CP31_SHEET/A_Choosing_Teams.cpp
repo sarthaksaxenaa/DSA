d4f739b593f54154ab9c545d20761ecd5a4f5f10
Sarthak090805 <sarthaksaxenaa05@gmail.com>
2026-07-18 16:54:41 +0530

    A Choosing Teams cp31 sheet code.

---
+++ CP31_SHEET/A_Choosing_Teams.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void solve(){
+    int a,b;
+    cin >> a >> b;
+    vector<int> nums(a);
+    int cnt =0;
+    for(int i=0; i<a; i++){
+        cin >> nums[i];
+        if(nums[i] + b <=5) cnt++;
+    }
+    cout << cnt/3;
+    
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+        solve();
+    return 0;
+}

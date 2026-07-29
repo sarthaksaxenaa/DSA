2429a012f27a42fe4cee44c3771648d62474eab8
Sarthak090805 <sarthaksaxenaa05@gmail.com>
2026-07-29 15:39:21 +0530

    A Trippi Troppi cp31 sheet code.

---
+++ CP31_SHEET/A_Trippi_Troppi.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void solve(){
+    string a,b,c;
+    cin >> a >> b >> c;
+
+    cout << a[0] << b[0] << c[0] << "\n";
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}

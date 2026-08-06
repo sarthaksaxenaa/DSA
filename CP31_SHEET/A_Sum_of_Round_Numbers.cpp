779212edf69c10aa78d5c63eb3a5472f1cc88b8a
Sarthak090805 <sarthaksaxenaa05@gmail.com>
2026-08-06 17:49:43 +0530

    A Sum of Round Numbers cp31 sheet code.

--- CP31_SHEET/A_Sum_of_Round_Numbers.cpp
+++ CP31_SHEET/A_Sum_of_Round_Numbers.cpp
@@ -2,41 +2,37 @@
 using namespace std;
 #define int long long
 
-void solve() {
+void solve(){
     int n;
     cin >> n;
 
     vector<int> ans;
     int place = 1;
 
-    while (n > 0) {
+    while(n){
         int digit = n % 10;
-
-        if (digit != 0) {
+        if(digit){
             ans.push_back(digit * place);
         }
-
-        place *= 10;
         n /= 10;
+        place *= 10;
     }
 
     cout << ans.size() << "\n";
-    reverse(ans.begin(), ans.end());
-    for (int x : ans) {
+    for(int x : ans){
         cout << x << " ";
     }
-
     cout << "\n";
 }
 
-int32_t main() {
+int32_t main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int t;
     cin >> t;
 
-    while (t--) {
+    while(t--){
         solve();
     }
 

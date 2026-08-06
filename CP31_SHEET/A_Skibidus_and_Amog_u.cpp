f0814655312afe42094987ea0d65d2eb9b9d520b
Sarthak090805 <sarthaksaxenaa05@gmail.com>
2026-08-06 18:02:49 +0530

    A Skibidus and Amog cp31 sheet code.

---
+++ CP31_SHEET/A_Skibidus_and_Amog_u.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void solve(){
+    string s;
+    cin >> s;
+
+    s.pop_back();
+    s.pop_back();
+
+    cout << s << "i\n";
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
